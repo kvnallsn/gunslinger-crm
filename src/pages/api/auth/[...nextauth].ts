686642721decaff23a6671ddb8c5bb312c60b731
kvnallsn <kvnallsn@gmail.com>
2023-02-20 21:21:33 +0000

    auth: verify user credentials

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,9 @@
-import { User } from "next-auth";
+import { getDatabaseConn } from "@/lib/db";
+import User from "@/lib/models/user";
+import { User as NextUser } from "next-auth";
 import NextAuth from "next-auth/next";
 import CredentialsProvider from "next-auth/providers/credentials";
+import * as argon2 from '@node-rs/argon2';
 
 export const authOptions = {
     providers: [
@@ -10,9 +13,29 @@ export const authOptions = {
                 email: { label: "Email" },
                 password: { label: "Password" },
             },
-            async authorize(credentials, req): Promise<User | null> {
-                const user = { id: "1", name: "Deltron Zero", email: "deltron@3030.com", image: '' };
-                return user ? user : null;
+            async authorize(credentials, req): Promise<NextUser | null> {
+                let user: NextUser | null = null;
+                const db = await getDatabaseConn();
+
+                try {
+                    if (!credentials) {
+                        throw new Error("missing credential fields");
+                    }
+
+                    const sysUser = await User.fetchUserByEmail(db, credentials.email);
+                    const hashed = await sysUser.fetchPassword(db);
+                    const success = await argon2.verify(hashed, credentials.password);
+
+                    if (success) {
+                        user = { id: sysUser.id, name: 'Deltron Zero', email: sysUser.email, image: '' };
+                    }
+                } catch (error: any) {
+                    console.error(error);
+                } finally {
+                    db.release();
+                }
+
+                return user;
             }
         })
     ],
